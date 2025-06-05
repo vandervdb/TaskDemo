572443a1f6d986b18ba4199b1eea543c2bff03b6
Arnaud  Vanderbecq <arnaud.vander@proton.me>
2025-06-05 09:26:57 +0200

    Add MobX store in the project - Add EsLint plugin to organize imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-duplicate-imports': 'warn',
+  },
 };
