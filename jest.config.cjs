b585364a14f8ba742593b4aa3d87b4b8b5d5ee8a
Arnaud Vanderbecq <vanderbeta-1@yahoo.fr>
2025-06-08 14:26:45 +0200

    chore: attempt to fix jest config

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,4 +3,7 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
   ],
+  transform: {
+    '^.+\\.[jt]sx?$': ['babel-jest', {configFile: './babel.config.cjs'}],
+  },
 };
