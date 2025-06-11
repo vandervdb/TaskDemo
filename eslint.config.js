1760c752b17e2f243c5048b3216238777a53593d
Arnaud  Vanderbecq <arnaud.vander@proton.me>
2025-06-11 10:54:22 +0200

    Replace ESLint configuration with React Native preset and test TaskStore

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,9 @@
-import js from '@eslint/js';
-import prettier from 'eslint-plugin-prettier';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import typescript from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import reactNative from '@react-native/eslint-config';
 
 /** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
   js.configs.recommended,
+  reactNative.flat, // <- ajouter ici
   {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
